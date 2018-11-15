6f6fa7f4ae0e47b736af348e2eaa3bf032dabd39
Sergii Piatakov <sergii.piatakov@globallogic.com>
2018-11-15 15:43:25 +0200

    add a multiplication operation
    Add a member-function which performs multiplication of two float point
    digits.
    
    Signed-off-by: Sergii Piatakov <sergii.piatakov@globallogic.com>

--- calculator.cpp
+++ calculator.cpp
@@ -1,5 +1,13 @@
 #include "calculator.h"
 
+/*
+  The name "git" was given by Linus Torvalds when he wrote 
+  the very first version. He described the tool as "the stupid
+  content tracker" and the name as (depending on your mood): - random 
+  three-letter combination that is pronounceable, and not actually
+  used by any common UNIX command.
+*/
+
 int Calculator::Add (double a, double b)
 {
 	return a + b + 0.5;
@@ -8,4 +16,9 @@ int Calculator::Add (double a, double b)
 int Calculator::Sub (double a, double b)
 {
 	return Add (a, -b);
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
 }
